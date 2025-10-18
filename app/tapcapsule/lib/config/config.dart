7d9b59bd929168d9d4718c3eff02c09dd36c17bf
davideblesse <102178095+davideblesse@users.noreply.github.com>
2025-10-19 01:22:23 +0200

    Remove deprecated config.dart with exposed API key

--- app/tapcapsule/lib/config/config.dart
+++
@@ -1,7 +0,0 @@
-class AppConfig {
-  static const String RPC_URL = 'https://base-sepolia.g.alchemy.com/v2/nprInoeFE5Wcp2pV7_PUu';
-
-  static const String CONTRACT_ADDRESS = '<PUT_CONTRACT_ADDRESS_HERE>';
-
-  static const String CONTRACT_ABI = '<PUT_ABI_JSON_HERE>';
-}
